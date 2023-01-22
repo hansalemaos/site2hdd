e015937cbbc226f05c758facfa133a54a12da6c0
Hans Alemão <aulasparticularesdealemaosp@gmail.com>
2023-01-22 10:26:12 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -21,6 +21,9 @@ from a_pandas_ex_apply_ignore_exceptions import pd_add_apply_ignore_exceptions
 pd_add_apply_ignore_exceptions()
 from url_analyzer import get_all_links_from_url, get_url_df, get_all_links_from_html
 
+import numba
+import windows_filepath
+import openpyxl
 
 @threadingbatch.thread_capture
 def get_url(
